ec775f0de6596d1a8c024776909891e7d6778d3e
Stan Cosmin <stancosminioan@gmail.com>
2024-06-15 01:25:49 +0300

    second commit for this banner

--- app.js
+++ app.js
@@ -2,17 +2,8 @@ const letf = document.querySelector(".left");
 const rigth = document.querySelector(".rigth");
 const box = document.querySelector(".container");
 const bodyEl = document.querySelector("body");
-/* box.addEventListener("mouseover", () => {
-  rigth.style.scale = "15%";
-});
-setTimeout(() => {
-  box.addEventListener("mouseover", () => {
-    rigth.style.scale = "100%";
-  });
-}, 1000); */
 
 box.addEventListener("mouseover", () => {
-  // bodyEl.classList.toggle("blured");
   bodyEl.style.backdropFilter = "blur(4px)";
   bodyEl.style.transition = "1s";
 });
@@ -20,7 +11,3 @@ box.addEventListener("mouseout", () => {
   bodyEl.style.backdropFilter = "none";
   bodyEl.style.transition = "1s";
 });
-
-// bodyEl.addEventListener("mouseover", () => {
-//   bodyEl.classList.remove(".blured");
-// });
